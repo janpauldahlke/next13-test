f63ae290a6b124500d522feb51b2f98d12bd5fe6
Jan Dahlke <jdahlke@aidminutes.com>
2023-02-14 09:37:26 +0100

    commented(out) queryBuiler

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 * seed
 * types
 * `npm run export`
+
+we wanted to add a generic QueryClient from your fellow youtuber to be able to ensure multiinstances
+[teh code](https://github.com/jherr/next13-use-warning/blob/main/app/page.tsx)
+[teh vid](https://youtu.be/zwQs4wXr9Bg?t=710)
--- components/LoremEntry/Entry.tsx
+++ components/LoremEntry/Entry.tsx
@@ -1,6 +1,26 @@
 import { seed as superSeed } from "@/seed"
 import { useEffect, useState } from "react";
 
+
+// we would need to consume an API here, for this example we will use a seed so it won't work
+// //setting up query client
+// function makeQueryClient() {
+//   const fetchMap = new Map<string, Promise<any>>();
+//   return function queryClient<QueryResult>(
+//     name: string,
+//     query: () => Promise<QueryResult>
+//   ): Promise<QueryResult> {
+//     if (!fetchMap.has(name)) {
+//       fetchMap.set(name, query());
+//     }
+//     return fetchMap.get(name)!;
+//   };
+// }
+
+
+// //creating query client
+// const queryClient = makeQueryClient();
+
 //interfacing type support on state
 export interface ILoremData {
   text: string;
